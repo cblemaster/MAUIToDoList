bca7da1540bd7e3d06ef5e892a81450c4811677b
Brian LeMaster <brian.lemaster@outlook.com>
2023-05-21 17:43:10 -0400

    Completed MAUI UI, pre-testing

--- MAUIToDoList.MAUI/MainPage.xaml.cs
+++ MAUIToDoList.MAUI/MainPage.xaml.cs
@@ -1,8 +1,24 @@
-﻿namespace MAUIToDoList.MAUI
+﻿using MAUIToDoList.Data;
+using MAUIToDoList.Services;
+using System.Collections.ObjectModel;
+
+namespace MAUIToDoList.MAUI
 {
     public partial class MainPage : ContentPage
     {
+        private readonly IRepository _db;
+        public MainPage(IRepository repository)
+        {
+            this.InitializeComponent();
 
-        public MainPage() => this.InitializeComponent();
+            this._db = repository;
+
+            this.BindingContext = this;
+        }
+
+        public ObservableCollection<ToDoItem> ToDoItems { get; set; }
+
+
+
     }
 }
