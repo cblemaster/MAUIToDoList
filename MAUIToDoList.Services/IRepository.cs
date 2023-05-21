bc29ee4ebc4bc6a0338df6f38679a1319186087a
Brian LeMaster <brian.lemaster@outlook.com>
2023-05-21 16:21:18 -0400

    Added repository interface and implementation to Services project

---
+++ MAUIToDoList.Services/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using MAUIToDoList.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIToDoList.Services
+{
+    public interface IRepository
+    {
+        ToDoItem? Get(int id);
+        IEnumerable<ToDoItem> GetAll();
+        ToDoItem Create(ToDoItem itemToCreate);
+        ToDoItem Update(ToDoItem itemToUpdate);
+        bool Delete(int id);
+    }
+}
---
+++ MAUIToDoList.Services/Repository.cs
@@ -0,0 +1,64 @@
+﻿using MAUIToDoList.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIToDoList.Services
+{
+    public class Repository : IRepository
+    {
+        private readonly SimpleToDoListContext _context;
+
+        public Repository() => this._context = new();
+
+
+        public ToDoItem? Get(int id) => this._context.ToDoItems.Find(id) ?? null;
+
+        public IEnumerable<ToDoItem> GetAll() => this._context.ToDoItems;
+        
+        public ToDoItem Create(ToDoItem itemToCreate)
+        {
+            try
+            {
+                if (itemToCreate == null) { throw new ArgumentException("The item to create doesn't exist.", nameof(itemToCreate)); }
+                if (itemToCreate.Id != 0) { throw new ArgumentException("The item to create cannot have an Id that isn't zero.", nameof(itemToCreate.Id)); }
+                
+                this._context.ToDoItems.Add(itemToCreate);
+                this._context.SaveChanges();
+            }
+            catch (Exception) { throw; }
+            return itemToCreate;
+        }
+        public ToDoItem Update(ToDoItem itemToUpdate)
+        {
+            try
+            {
+                if (itemToUpdate == null) { throw new ArgumentException("The item to update doesn't exist.", nameof(itemToUpdate)); }
+                if (itemToUpdate.Id == 0) { throw new ArgumentException("The item to update cannot have an Id that is zero.", nameof(itemToUpdate)); }
+
+                this._context.ToDoItems.Update(itemToUpdate);
+                this._context.SaveChanges();
+            }
+            catch (Exception) { throw; }
+            return itemToUpdate;
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                if (id <= 0) { throw new ArgumentException("The item to delete cannot have an Id that is less than or equal to zero.", nameof(id)); }
+
+                ToDoItem? found = this.Get(id);
+
+                if (found == null) { throw new Exception("The item to delete doesn't exist."); }
+
+                this._context.ToDoItems.Remove(found);
+                this._context.SaveChanges();
+            }
+            catch (Exception) { throw; }
+            return true;
+        }
+    }
+}
