d811b84f4cdd45d2be0581574c381312f2dad4a1
Brian LeMaster <brian.lemaster@outlook.com>
2023-05-21 15:53:40 -0400

    Added scaffolded EF datacontext and model classes to Data project

---
+++ MAUIToDoList.Data/SimpleToDoListContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MAUIToDoList.Data;
+
+public partial class SimpleToDoListContext : DbContext
+{
+    public SimpleToDoListContext()
+    {
+    }
+
+    public SimpleToDoListContext(DbContextOptions<SimpleToDoListContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ToDoItem> ToDoItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=simple_to_do_list;Trusted_Connection=True;Trust Server Certificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ToDoItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_transfer_types");
+
+            entity.ToTable("to_do_item");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("description");
+            entity.Property(e => e.DueDate)
+                .HasColumnType("datetime")
+                .HasColumnName("due_date");
+            entity.Property(e => e.IsComplete).HasColumnName("is_complete");
+            entity.Property(e => e.IsImportant).HasColumnName("is_important");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MAUIToDoList.Data/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MAUIToDoList.Data;
+
+public partial class ToDoItem
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public bool IsImportant { get; set; }
+
+    public bool IsComplete { get; set; }
+
+    public DateTime? DueDate { get; set; }
+}
