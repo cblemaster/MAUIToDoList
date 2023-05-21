4baf31ce58af247c99950924af9cacce449a009c
Brian LeMaster <brian.lemaster@outlook.com>
2023-05-21 16:49:21 -0400

    Roughed out UI in MAUI project

--- MAUIToDoList.MAUI/MauiProgram.cs
+++ MAUIToDoList.MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MAUIToDoList.Services;
+using Microsoft.Extensions.Logging;
 
 namespace MAUIToDoList.MAUI
 {
@@ -13,7 +14,8 @@ namespace MAUIToDoList.MAUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .Services.AddSingleton<IRepository, Repository>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
