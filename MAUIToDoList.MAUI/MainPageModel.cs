ede15cc31d349c7a4cf1fe8795453aa2575d9359
Brian LeMaster <brian.lemaster@outlook.com>
2023-05-24 15:01:36 -0400

    Added viewmodel to UI in MAUI project

---
+++ MAUIToDoList.MAUI/MainPageModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MAUIToDoList.Data;
+using System.Collections.ObjectModel;
+
+namespace MAUIToDoList.MAUI
+{
+    public class MainPageModel : ObservableObject
+    {
+        private readonly SimpleToDoListContext _context = new();
+        private ObservableCollection<ToDoItem> _toDoItems = new();
+        private ToDoItem? _selectedToDo = null;
+
+        public MainPageModel()
+        {
+            this.ToDoItems = this.GetAllToDoItems();
+        }        
+
+        public ObservableCollection<ToDoItem> ToDoItems
+        {
+            get => _toDoItems;
+            set => SetProperty(ref _toDoItems, value);
+        }
+
+        public ToDoItem? SelectedToDo
+        {
+            get => _selectedToDo;
+            set => SetProperty(ref _selectedToDo, value);
+        }
+
+        public ObservableCollection<ToDoItem> GetAllToDoItems() => new ObservableCollection<ToDoItem>(this._context.ToDoItems.OrderBy(t => t.DueDate).ThenBy(t => t.Name));
+
+        public ObservableCollection<ToDoItem> GetIncompleteToDoItems() => new ObservableCollection<ToDoItem>(this._context.ToDoItems.Where(t => !t.IsComplete).OrderBy(t => t.DueDate).ThenBy(t => t.Name));
+
+
+    }
+}
